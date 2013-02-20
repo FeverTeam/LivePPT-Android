6a00e75b7ba016fc749ddc6c1ee3287a751356f6
lixingji <799658082@qq.com>
2013-02-20 19:20:44 +0800

    在MainActivity中实现tab的基本功能。
    在layout添加一个tabs的布局,用tabhost布局位置，在MainActivity布置实现tab的代码

--- LivePPT/src/fever/liveppt/MainActivity.java
+++ LivePPT/src/fever/liveppt/MainActivity.java
@@ -1,23 +1,27 @@
 package fever.liveppt;
 
+import android.app.TabActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.view.Window;
+import android.widget.TabHost;
 
-public class MainActivity extends Activity {
+public class MainActivity extends TabActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tabs);
+		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,R.layout.tabs); 
+		TabHost tabhost = getTabHost();
+		tabhost.addTab(tabhost.newTabSpec("tab1").setIndicator("PPT")
+				.setContent(R.id.tab_PPT));
+		tabhost.addTab(tabhost.newTabSpec("tab2").setIndicator("Meeting")
+				.setContent(R.id.tab_Meeting));
+		tabhost.addTab(tabhost.newTabSpec("tab3").setIndicator("Live")
+				.setContent(R.id.tab_Live));
+		tabhost.addTab(tabhost.newTabSpec("tab4").setIndicator("more")
+				.setContent(R.id.tab_more));
+	}
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
 }
